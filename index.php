366a6b1f50a758f76233cd793aa54084978cbfdd
Duex <deux02030@gmail.com>
2017-11-13 17:50:34 -0500

    docs: deleted external css and added to index file.
    Added styles to div and body elements.

--- index.php
+++ index.php
@@ -8,30 +8,57 @@ if(isset($_POST['add-item'])) {
     if(mysqli_query($connection, $query)) {
         echo "<h3>You have added an item to the database</h3>";
     }
-}
-
-$query1 = mysqli_query($connection, "SELECT * FROM tasks");
-
-while($results = mysqli_fetch_array($query1)) {
-    echo "\n" . $results['id'] . "\n" . $results['task'];
+    $query1 = mysqli_query($connection, "SELECT * FROM tasks");
+    
+    while($results = mysqli_fetch_array($query1)) {
+        echo "\n" . $results['id'] . "\n" . $results['task'];
+    }
 }
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width.initial-scale=1">
-    <title>Task Application</title>
-    <link rel="stylesheet" href="css/style.css">
+        <meta charset="UTF-8">
+        <title>Task Application</title>
+        <style>
+            body {
+                background-color: lightgray;
+                width: 100%;
+                max-width: 600px;
+                margin: 20px auto;
+            }
+            .container {
+                background-color: white;
+                padding: 20px;
+            }
+            .tasks {
+                text-align: center;
+            }
+            ul {
+                -webkit-padding-start: 0;
+                list-style-type: none;
+            }
+            input {
+                width: 100%;
+            }
+        </style>
     </head>
     <body>
         <div class="container">
-        <form action="index.php" method="POST">
-            <input type="text" name="name">
-            <br>
-            <input type="submit" name="add-item" value="Add Item">
-        </form>
+            <div class="heading">
+                <h1 align="center">Tasks</h1>
+            </div>
+            <div class="tasks">
+                <ul>
+                    <li>A task</li>
+                </ul>
+            </div>
+            <form action="index.php" method="POST">
+                <input align: "center" type="text" name="name">
+                <br>
+                <input type="submit" name="add-item" value="Add Item">
+            </form>
         </div>
     </body>
 </html>
